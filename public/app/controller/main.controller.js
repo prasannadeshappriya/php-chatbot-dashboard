a5be2e75188c02e691362fb1d409b0e12ecfe66a
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-27 23:11:03 +0530

    Implement dashboard view

--- public/app/controller/main.controller.js
+++ public/app/controller/main.controller.js
@@ -1,9 +1,45 @@
 /**
  * Created by prasanna on 8/27/17.
  */
-app.controller('mainController',['$scope',function ($scope) {
-    $scope.test = 'This is prasanna god';
+app.controller('mainController',[
+    '$scope', '$http',
+    function ($scope, $http) {
+    let version = '20170307';
+    $scope.arrResponse = {};
     $scope.onInit = function () {
         console.log('Initialization works');
-    }
+    };
+    $scope.btnSubmitClick = async function () {
+        try {
+            let request = await $http({
+                method: "GET",
+                url: 'https://api.wit.ai/message?v=' + version + '&q=' + $scope.userInput,
+                headers: {
+                    Authorization: 'Bearer 6PN2II4QPW5UYG3VPR6EXWFRU6MTTFBH'
+                }
+            });
+            if(request){
+                let entities = request.data.entities;
+                let retObj = [];
+                Object.keys(entities)
+                    .forEach(function eachKey(key) {
+                        let item = entities[key];
+                        for(let j=0; j<Object.keys(item).length; j++){
+                            let entity_item = item[Object.keys(item)[j]];
+                            let tmpObj = {};
+                            tmpObj['entity_name'] = key;
+                            Object.keys(entity_item)
+                                .forEach(function eachKey(key) {
+                                    tmpObj[key] = entity_item[key];
+                                });
+                            retObj.push(tmpObj);
+                        }
+                    });
+                $scope.arrResponse = retObj;
+                $scope.$apply();
+            }
+        }catch (err){
+            console.log(err);
+        }
+    };
 }]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,22 +16,31 @@
 <div class="container">
     <div class="panel panel-default" style="margin-top: 20px">
         <div class="panel-heading">
-            <span class="text-info"><b>Input any command</b></span>
+            <span class="text-info"><b>Verify commands and entities</b></span>
         </div>
         <div class="panel-body">
             <div class="form-group col-lg-6 col-lg-offset-3">
                 <label for="lblMain" class="control-label">Command</label>
-                <input type="text" id="lblMain" class="form-control">
+                <input type="text" id="lblMain" class="form-control" ng-model="userInput">
             </div>
             <div class="col-lg-12">
-                <input type="button" class="btn btn-success center-block" value="Submit" style="width:150px;">
+                <input type="button" class="btn btn-success center-block" value="Submit" ng-click="btnSubmitClick()" style="width:150px;">
             </div>
             <div class="form-group col-lg-6 col-lg-offset-3">
-                <textarea style="width: 100%; margin-top: 3%; resize: none; height: 200px"></textarea>
-            </div>
-            <div class="form-group col-lg-6 col-lg-offset-3">
-                <p>This is a test <br>
-                @{{test}}</p>
+                <div ng-repeat="item in arrResponse track by $index">
+                    <div ng-if="$index===0">
+                        <hr>
+                    </div>
+                    <div ng-repeat="(key, value) in item track by $index">
+                        <div ng-if="$index===0">
+                            <h3 class="text-info">Entity:- @{{value}}</h3>
+                        </div>
+                        <div ng-if="$index!==0" style="margin-left: 10%">
+                            <p>@{{key}} - @{{value}}</p>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
             </div>
         </div>
     </div>
