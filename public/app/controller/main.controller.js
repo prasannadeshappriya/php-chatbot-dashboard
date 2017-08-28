7a4913fb1e91fafa6ce8b6cda18193673f58215b
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-28 18:32:11 +0530

    Code improvements

--- public/app/controller/main.controller.js
+++ public/app/controller/main.controller.js
@@ -5,10 +5,45 @@ app.controller('mainController',[
     '$scope', '$http',
     function ($scope, $http) {
     let version = '20170307';
+
     $scope.arrResponse = {};
-    $scope.onInit = function () {
-        console.log('Initialization works');
+    $scope.arrEntities = [];
+    $scope.arrWitEntities = [];
+
+    $scope.onInit = async function () {
+        try {
+            let request = await $http({
+                method: "GET",
+                url: 'https://api.wit.ai/entities?v=20170307',
+                headers: {
+                    Authorization: 'Bearer 6PN2II4QPW5UYG3VPR6EXWFRU6MTTFBH',
+                }
+            });
+            if(request){
+               let data = request.data;
+               data.forEach(function (value) {
+                   let tmpObj = {}; let con = true;
+                   for(let i=0; i<value.length; i++){
+                       if(value[i]==='$'){
+                           tmpObj.entity = value.slice(0, i) + '/' + value.slice(i+1);
+                           con=false; break;
+                       }
+                   }
+                   if(con) {
+                       tmpObj.wit = false; tmpObj.entity = value;
+                       $scope.arrEntities.push(tmpObj);
+                   }else{
+                       tmpObj.wit = true;
+                       $scope.arrWitEntities.push(tmpObj);
+                   }
+               });
+               $scope.$apply();
+            }
+        }catch (err){
+            console.log(err);
+        }
     };
+
     $scope.btnSubmitClick = async function () {
         try {
             let request = await $http({
@@ -36,6 +71,7 @@ app.controller('mainController',[
                         }
                     });
                 $scope.arrResponse = retObj;
+                console.log(retObj);
                 $scope.$apply();
             }
         }catch (err){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,32 +14,55 @@
 </head>
 <body ng-app="app" ng-controller="mainController" ng-init="onInit()">
 <div class="container">
-    <div class="panel panel-default" style="margin-top: 20px">
-        <div class="panel-heading">
-            <span class="text-info"><b>Verify commands and entities</b></span>
+    <div class="col-lg-12" style="margin-top: 20px">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span class="text-info"><b>Verify commands and entities</b></span>
+                </div>
+                <div class="panel-body">
+                    <div class="form-group col-lg-6 col-lg-offset-3">
+                        <label for="lblMain" class="control-label">Command</label>
+                        <input type="text" id="lblMain" class="form-control" ng-model="userInput">
+                    </div>
+                    <div class="col-lg-12">
+                        <input type="button" class="btn btn-success center-block" value="Submit" ng-click="btnSubmitClick()" style="width:150px;">
+                    </div>
+                    <div class="form-group col-lg-6 col-lg-offset-3">
+                        <div ng-repeat="item in arrResponse track by $index">
+                            <div ng-if="$index===0">
+                                <h4 class="text-info">Entities </h4>
+                                <hr>
+                            </div>
+                            <div ng-repeat="(key, value) in item track by $index">
+                                <div ng-if="$index===0">
+                                    <h4 class="text-info">@{{$index +1 }}) @{{value}}</h4>
+                                </div>
+                                <div ng-if="$index!==0" style="margin-left: 10%">
+                                    <p>@{{key}} - @{{value}}</p>
+                                </div>
+                                <div class="progress" ng-if="key==='confidence'" style="margin-left: 10%; width: 80%">
+                                    <div class="progress-bar progress-bar-success" style="width: @{{value*100}}%"></div>
+                                </div>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-            <div class="form-group col-lg-6 col-lg-offset-3">
-                <label for="lblMain" class="control-label">Command</label>
-                <input type="text" id="lblMain" class="form-control" ng-model="userInput">
-            </div>
+        <div class="col-lg-6" style="border-left: 1px solid #d3d3d3; padding-left: 50px">
+            <h4 class="text-info">App Entities</h4>
+            <h5 class="text-info">Entities: </h5>
             <div class="col-lg-12">
-                <input type="button" class="btn btn-success center-block" value="Submit" ng-click="btnSubmitClick()" style="width:150px;">
+                <div class="col-lg-6" ng-repeat="item in arrEntities track by $index">
+                    <p>@{{$index+1}}) @{{item.entity}}</p>
+                </div>
             </div>
-            <div class="form-group col-lg-6 col-lg-offset-3">
-                <div ng-repeat="item in arrResponse track by $index">
-                    <div ng-if="$index===0">
-                        <hr>
-                    </div>
-                    <div ng-repeat="(key, value) in item track by $index">
-                        <div ng-if="$index===0">
-                            <h3 class="text-info">Entity:- @{{value}}</h3>
-                        </div>
-                        <div ng-if="$index!==0" style="margin-left: 10%">
-                            <p>@{{key}} - @{{value}}</p>
-                        </div>
-                    </div>
-                    <hr>
+            <h5 class="text-info">Wit Entities: </h5>
+            <div class="col-lg-12">
+                <div class="col-lg-6" ng-repeat="item in arrWitEntities track by $index">
+                    <p>@{{$index+1}}) @{{item.entity}}</p>
                 </div>
             </div>
         </div>
